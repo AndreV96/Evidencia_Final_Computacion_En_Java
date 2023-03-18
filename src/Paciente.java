7d57d56dfdee79dd201845379a50f2059874dd4f
Andre Vega <andrevega@Andres-MacBook-Pro.local>
2023-03-17 20:19:01 -0600

    Se creó la clase básica de Paciente

--- src/Paciente.java
+++ src/Paciente.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Paciente {
+import java.util.UUID;
+
+public class Paciente {
+
+    private UUID id;
+    private String nombre;
+
+    public Paciente(String nombre, String especialidad) {
+        this.id = UUID.randomUUID();
+        this.nombre = nombre;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 }
