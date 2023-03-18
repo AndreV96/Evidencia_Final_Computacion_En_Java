f65385de09aa32d05c6b8e1467cd95c0ace7df98
Andre Vega <andrevega@Andres-MacBook-Pro.local>
2023-03-17 23:04:20 -0600

    Creacion de lectores y escritores de archivos y de metodo de inicio y de login

--- src/Paciente.java
+++ src/Paciente.java
@@ -5,8 +5,13 @@ public class Paciente {
     private UUID id;
     private String nombre;
 
-    public Paciente(String nombre, String especialidad) {
+    public Paciente(String nombre) {
         this.id = UUID.randomUUID();
+        this.nombre = nombre;
+    }
+
+    public Paciente(UUID id, String nombre) {
+        this.id = id;
         this.nombre = nombre;
     }
 
